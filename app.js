537b107e96c8c70ad5102083ea66d0ba3d6a3847
Del4xuw <Kevintorrezupds@gmail.com>
2022-02-16 00:28:15 -0400

    a

--- app.js
+++ app.js
@@ -1,11 +1,2 @@
-var i = 0;
-    var txt = 'Esta es una pagina con un poco de javascript';
-    var speed = 80;
-    function typeWrite(){
-        if(i < txt.length){
-            document.getElementById ('texto').innerHTML += txt.charAt(i);
-            i++;
-            setTimeout (typeWrite, speed);
-        }
-    }
+
 <h1 id="texto"></h1>
