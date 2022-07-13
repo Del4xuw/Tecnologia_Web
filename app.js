6c41a9e774ae073ce14e61dd4a6cc383036633d7
Del4xuw <Kevintorrezupds@gmail.com>
2022-07-13 09:33:51 -0400

    A

---
+++ app.js
@@ -0,0 +1,21 @@
+
+
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 100){
+        document.querySelector('.go-top-container')
+        .classList.add('show');
+    }else{
+        document.querySelector('.go-top-container')
+        .classList.remove('show');
+    }
+}
+
+document.querySelector('.go-top-container')
+.addEventListener('click', () =>{
+    window.scrollTo({
+        top:1,
+        behavior:'smooth'
+    });
+});
+
+
